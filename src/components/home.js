04ab76fe80c1d0bce6ab1b212a1d117ff8b4f028
Natanael Figueiredo <natanael.crim@gmail.com>
2025-06-11 20:48:51 -0400

    Correção botão download da Roleta

--- src/components/home.js
+++ src/components/home.js
@@ -1,39 +1,65 @@
-import React, {useState} from "react";
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const urlToDownload = 'https://www.dropbox.com/scl/fi/8wjcfsqn2c3x31b7u57w5/roleta-matematica.apk?rlkey=j526g961zzc2419fas1yf1wpg&st=lpvw65dk&dl=1'
-export default function Home () {
+const urlToDownload = 'https://www.dropbox.com/scl/fi/8wjcfsqn2c3x31b7u57w5/roleta-matematica.apk?rlkey=j526g961zzc2419fas1yf1wpg&st=lpvw65dk&dl=1';
+
+export default function Home() {
     const [download, setDownload] = useState('');
-    const [buttonActive, setbuttonActive] = useState (false);
+    const [buttonActive, setButtonActive] = useState(false);
 
     const handleClick = () => {
         setDownload(urlToDownload);
-        setbuttonActive(true);
-    }
+        setButtonActive(true);
+
+        // Forçar o navegador a iniciar o download automaticamente
+        window.location.href = urlToDownload;
+    };
 
     return (
-        <div class="container">
-        <div class="d-flex flex-column wrapper">
-        <div className="input-box bg-secondary">
-            <h1  style={{color: "red", background: "black"}}><div className="App"><img src="/logomhs.png" width={100} alt="MHS"></img>
-                Sejam Bem Vindos <img src="/background2.png" width={150}></img> </div>
-                <p>Math High Speed</p></h1>
-            <p><a href="sign-in">
-                    <button className="btn btn-info">LOGAR</button>
-                </a> para Jogar MHS no PC
-            </p>
-            <p>Logar e entrar para o Ranking Mundial</p>
-            
-            <p>Instalar ROLETA MATEMÁTICA no Celular Android
-                <p><button className="btn btn-danger" disabled={buttonActive} onClick={handleClick}>Download do APK</button></p>
-                {download && <p style={{color:"white"}}> Aguarde o Download.</p>};
-                <p>{download}</p>
-                {download &&  <iframe src={download}></iframe>}
-            </p>
+        <div className="container">
+            <div className="d-flex flex-column wrapper">
+                <div className="input-box bg-secondary p-4 rounded">
+                    <h1 style={{ color: "red", background: "black", padding: "10px" }}>
+                        <div className="App d-flex align-items-center gap-3">
+                            <img src="/logomhs.png" width={100} alt="MHS" />
+                            Sejam Bem Vindos
+                            <img src="/background2.png" width={150} alt="" />
+                        </div>
+                        <p>Math High Speed</p>
+                    </h1>
+
+                    <p>
+                        <Link to="/sign-in">
+                            <button className="btn btn-info">LOGAR</button>
+                        </Link>{" "}
+                        para Jogar MHS no PC
+                    </p>
+
+                    <p>Logar e entrar para o Ranking Mundial</p>
+
+                    <p>Instalar ROLETA MATEMÁTICA no Celular Android</p>
+                    <p>
+                        <button
+                            className="btn btn-danger"
+                            disabled={buttonActive}
+                            onClick={handleClick}
+                        >
+                            Download do APK
+                        </button>
+                    </p>
+
+                    {download && <p style={{ color: "white" }}>Aguarde o Download...</p>}
+
+                    {download && (
+                        <p>
+                            <a href={download} className="btn btn-success" download>
+                                Clique aqui se o download não iniciar automaticamente
+                            </a>
+                        </p>
+                    )}
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-    )
+    );
 }
